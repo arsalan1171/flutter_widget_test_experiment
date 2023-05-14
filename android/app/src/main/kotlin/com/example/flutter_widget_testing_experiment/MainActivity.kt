862637cff50498a4f69819a223ec1d65ec486c86
arsalan665@gmail.com <arsalan665@gmail.com>
2023-05-14 12:46:27 +0200

    widget testing of static text label, button tap and text that gets updated

---
+++ android/app/src/main/kotlin/com/example/flutter_widget_testing_experiment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_widget_testing_experiment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    super.key,
+    required this.message,
+  });
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(message));
+  }
+}
